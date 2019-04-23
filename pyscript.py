1e4fcb9722507a23f4e36caf737af3804194e2d6
williansoaress <mr.williansilva@gmail.com>
2019-04-23 11:57:12 -0300

    Guarda a média do tempo de execução do benchmark bt numa variável do tipo float

--- pyscript.py
+++ pyscript.py
@@ -1,9 +1,16 @@
 import pandas as pd
 
+#Atribui o arquivo csv para as variaveis
 dataBt = pd.read_csv('/home/willian/MeuScript/WillianSoares/Resultado/bt.S.csv')
-dataIs = pd.read_csv('/home/willian/MeuScript/WillianSoares/Resultado/is.S.csv')
-dataFt = pd.read_csv('/home/willian/MeuScript/WillianSoares/Resultado/ft.S.csv')
+#dataIs = pd.read_csv('/home/willian/MeuScript/WillianSoares/Resultado/is.S.csv')
+#dataFt = pd.read_csv('/home/willian/MeuScript/WillianSoares/Resultado/ft.S.csv')
 
-print (dataBt.mean())
-print (dataIs.mean())
-print (dataFt.mean())
+#Calcula a media dos valores na coluna "timeExec" e atribui o resultado a variavel como float
+btmedia = dataBt['timeExec'].mean()
+
+#Imprime o resultado doa media
+print btmedia
+
+#print (dataBt.mean())
+#print (dataIs.mean())
+#print (dataFt.mean())
